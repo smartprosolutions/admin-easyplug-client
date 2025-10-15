869e81627561a2c3e9dc37fb728491f8f1d53837
Kamzen <37660966+Kamzen@users.noreply.github.com>
2025-10-15 21:22:22 +0200

    Create Advertisements.jsx

---
+++ src/pages/Advertisements.jsx
@@ -0,0 +1,12 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+export default function Advertisements() {
+  return (
+    <Box>
+      <Typography variant="h5">Advertisements</Typography>
+    </Box>
+  );
+}
+
+
